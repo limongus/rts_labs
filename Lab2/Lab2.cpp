c8a663a64db0ec1baf7723638fe5c5c1559930f0
limongus <145297656+limongus@users.noreply.github.com>
2025-11-25 17:12:06 +0200

    Лабораторная работа №2

---
+++ Lab2/Lab2.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <string>
+#include <thread>
+#include <mutex>
+#include <chrono>
+#include <clocale>
+
+std::mutex m;
+
+void Func(std::string name) {
+    long double i = 0;
+    auto start = std::chrono::steady_clock::now();
+    while (std::chrono::steady_clock::now() - start < std::chrono::seconds(1)) {
+        i += 1e-9;
+    }
+
+    //m.lock();
+    std::cout << name << ": " << i << std::endl;
+    //m.unlock();
+}
+
+int main() {
+    setlocale(LC_ALL, "RU");
+
+    std::thread thread1(Func, "t1");
+    std::thread thread2(Func, "t2");
+    std::thread thread3(Func, "t3");
+
+    thread1.join();
+    thread2.join();
+    thread3.join();
+
+    system("pause");
+    return 0;
+}
